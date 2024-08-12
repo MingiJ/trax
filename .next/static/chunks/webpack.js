23246f73362487094e519640484378cbaa178c05
MingiJ <jeffawesome.ja@gmail.com>
2024-08-12 17:29:12 +0300

    Create menu's on sidebar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cb6336c3a7442b2d"; }
+/******/ 		__webpack_require__.h = function() { return "8a3e3d26dd7670dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
